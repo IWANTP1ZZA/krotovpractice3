b933701b55ab9fff55a73958051c1412454f1f2f
IWANTP1ZZA <71212366+IWANTP1ZZA@users.noreply.github.com>
2021-06-01 15:25:05 +0300

    Initial commit

---
+++ IntentApp/src/main/java/ru/mirea/krotovpractice3/SecondActivity.java
@@ -0,0 +1,18 @@
+package ru.mirea.krotovpractice3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.SecondActivity);
+        String date = (String) getIntent().getSerializableExtra("date");
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(date);
+    }
+}
